0a18f7afb788821b58a1124172b73e3b1b5d734b
Iponeatdog <152945377+Iponeatdog@users.noreply.github.com>
2026-06-03 09:59:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import smtplib
 import os
 
 API_KEY = os.environ.get("OWM_API_KEY")
-LAT = 13.756331
-LON = 100.501762
+LAT = 13.721860
+LON = 100.776459
 
 my_email = os.environ.get("EMAIL")
 password = os.environ.get("PASSWORD")
